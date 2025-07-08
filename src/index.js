06d90c8166383968f7d7c9ce72fbd0441ddf9175
cmatyas <119801952+cmatyas@users.noreply.github.com>
2025-07-08 14:18:21 -0400

    Improved User Experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 function displayRelatedPhenom(response) {
-  console.log(response);
+
+    let generatingNotice = document.querySelector("#notice");
+    generatingNotice.classList.add("hidden"); 
+
+    let relatedPhenom = document.querySelector("#generation")
+    relatedPhenom.classList.remove("hidden");
+
     let relatedPhenomOne = document.querySelector("#related-phenomenon-one");
     let relatedPhenomTwo = document.querySelector("#related-phenomenon-two");
     let relatedPhenomThree = document.querySelector("#related-phenomenon-three");
@@ -22,10 +28,12 @@ function generateRelatedPhenom(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "dea7930290bf9064796ot2c1b9b4c9a7";
-    let context = "You are an expert in the Next Generation Science Standards, OpenSciEd pedagogy, and science storylines. You know what phenomena would be interesting to students."
-    let prompt = `User instructions: Please generate a list of three phenomena related to ${instructionsInput.value}. Include $$ between each phenomenon example.`;
+    let context = "You are an expert in the Next Generation Science Standards, OpenSciEd pedagogy, and science storylines. You know what phenomena would be interesting to students and those that could span an entire science unit."
+    let prompt = `User instructions: Please generate a list of three phenomena related to ${instructionsInput.value}. Include $$ between each phenomenon example. Please do not include any introductory text or other information besides the three examplees.`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
+  
+    let generatingRow = document.querySelector("#results");
+    generatingRow.classList.remove("hidden");  
 
     axios.get(apiURL).then(splitString)
 }
