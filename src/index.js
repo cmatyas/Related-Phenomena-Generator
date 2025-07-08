ef29e24d05a40593b84484fa682bef73452a214e
cmatyas <119801952+cmatyas@users.noreply.github.com>
2025-07-08 11:58:46 -0400

    Added AI API Integration and created three columns for the response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+function displayRelatedPhenom(response) {
+  console.log(response);
+    let relatedPhenomOne = document.querySelector("#related-phenomenon-one");
+    let relatedPhenomTwo = document.querySelector("#related-phenomenon-two");
+    let relatedPhenomThree = document.querySelector("#related-phenomenon-three");
+    relatedPhenomOne.innerHTML = response[0];
+    relatedPhenomTwo.innerHTML = response[1];
+    relatedPhenomThree.innerHTML = response[2];
+}
+
+
+function splitString(response) {
+  let relatedPhenomena = response.data.answer
+  const newPhenom = relatedPhenomena.split("$$");
+    console.log(newPhenom);
+  displayRelatedPhenom(newPhenom);
+}
+
+
 function generateRelatedPhenom(event) {
     event.preventDefault();
 
-    let relatedPhenom = document.querySelector("#related-phenom");
-    relatedPhenom.innerHTML = "List of Related Phenomena will go here.";
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "dea7930290bf9064796ot2c1b9b4c9a7";
+    let context = "You are an expert in the Next Generation Science Standards, OpenSciEd pedagogy, and science storylines. You know what phenomena would be interesting to students."
+    let prompt = `User instructions: Please generate a list of three phenomena related to ${instructionsInput.value}. Include $$ between each phenomenon example.`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+    axios.get(apiURL).then(splitString)
 }
 
 let phenomFormElement = document.querySelector("#phenom-generator-form");
